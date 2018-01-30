144a717dde2ef6e1b0339e76d29940a524f541f2
Lindsay Maher <lindsaymah3r@gmail.com>
2018-01-30 12:00:28 -0500

    adding files to remote

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('Yaaaaas queen 👑')
+console.log('Yaaaaas queen 👑');
+
+const mapboxgl = require('mapbox-gl');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWxla3NzaGluZWxldmEiLCJhIjoiY2pkMXZnYW91MGlzcjJ3cGExYWplZDBudCJ9.ZJyH5Obt-EYKod3LgmBwYw';
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+    zoom: 12, // starting zoom
+    style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available
+});
+
